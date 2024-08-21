a2bc7fc96c0adca95a3c4316b70bacf046a8e757
Alejandro <ale.robledo1447@gmail.com>
2024-08-21 17:07:35 -0300

    Interacciones con jQuery UI

--- aprendiendo-jquery/js/06 - interface.js
+++ aprendiendo-jquery/js/06 - interface.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-    console.log("hola");
+    //Me permite mover elementos por toda la pantalla
+    $(".elemento").draggable(); 
+
+    //Redimensionar los elementos
+    $(".elemento").resizable(); 
+
+    //Seleccionar y ordenar elementos
+   /*  $(".lista-seleccionable").selectable(); */
+    $(".lista-seleccionable").sortable();
+
+    //Drop
+    $("#elemento-movido").draggable();
+    $("#area").droppable({
+        drop: function(){
+            console.log("Has soltado algo dentro del area")
+        }
+    });
+
 });
